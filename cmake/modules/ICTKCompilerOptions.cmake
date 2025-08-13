b04470efb7f8daa85d024702dbdaf240bcd6797c
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-08-13 21:05:12 +0530

    cmake

--- cmake/modules/ICTKCompilerOptions.cmake
+++ cmake/modules/ICTKCompilerOptions.cmake
@@ -9,6 +9,38 @@ function (ictk_apply_compiler_options tgt)
         /W4 /WX
         /Zc:preprocessor /Zc:__cplusplus
         $<$<BOOL:${ICTK_NO_RTTI}>: /GR->
+        # MSVC dont truly disable EH, so we enforce "no throw" in code when NO_EXCEPTIONS = 1
         /EHsc
     ) 
-    # LTO ib MSVC
+    # LTO in MSVC
+    target_compile_options(${tgt} INTERFACE
+      $<$AND:$<CONFIG:Release>,$<BOOL:${ICTK_ENABLE_LTO}>>:/GL>
+    ) 
+
+    target_link_options(${tgt} INTERFACE
+      $<$<AND:$<CONFIG:Release>,$<BOOL:${ICTK_ENABLE_LTO}>>:/LTCG>
+    )
+  
+  else()
+    # GCC/Clang
+    target_compile_options(${tgt} INTERFACE
+      -Wall -Wextra -Wpedantic -Werror
+      -Wconversion -Wdouble-promotion -Wshadow -Wnon-virtual-dtor -Wold-style-cast
+      -ffp-contract=off -fno-math-errno -fno-signed-zeros
+      $<$<BOOL:${ICTK_NO_RTTI}>:-fno-rtti>
+      $<$<BOOL:${ICTK_NO_EXCEPTIONS}>:-fno-exceptions>
+    )
+
+    # LTO for release when requested
+    target_compile_options(${tgt} INTERFACE
+      $<$<AND:$<CONFIG:Release>,$<BOOL:${ICTK_ENABLE_LTO}>>:-flto>
+    )
+  
+  endif()
+
+
+  ## Gen a compile_commands.json for tooling
+
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE BOOL "Export compile commands for tooling" FORCE)
+
+endfunction()
---
+++ cmake/modules/ICTKSanitizers.cmake
@@ -0,0 +1,37 @@
+## Applying sanitizers
+function(ictk_apply_sanitizers tgt)
+    ## Sanitizers only for debug builds not for release builds
+    if (NOT CMAKE_BUILD_TYPE MATCHES "Debug")
+        return()
+    endif()
+
+    ## MSVC: skip Asan/UBsan support is limited/variant-dependent
+    if (MSVC)
+        return()
+    endif()
+
+    ## AddressSanitizer
+    if (ICTK_ENABLE_ASAN)
+        target_compile_options(${tgt} INTERFACE
+            -fsanitize=address
+            -fno-omit-frame-pointer
+        )
+
+        target_link_options(${tgt} INTERFACE
+            -fsanitize=address
+        )
+    endif()
+
+    ## UndefinedBehaviorSanitizer
+    if (ICTK_ENABLE_UBSAN)
+        target_compile_options(${tgt} INTERFACE
+            -fsanitize=undefined
+            -fno-omit-frame-pointer
+        )
+
+        target_link_options(${tgt} INTERFACE
+            -fsanitize=undefined
+        )
+    endif()
+
+endfunction()
