ecc5af5da5f08024cbac371b8d9408541d9fd9f6
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-08-15 13:33:06 +0530

    version

---
+++ include/ictk/version.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#define ICTK_VERSION_MAJOR 0
+#define ICTK_VERSION_MINOR 0
+#define ICTK_VERSION_PATCH 2
+#define ICTK_C_ABI_VER 1
+
+
+namespace ictk{
+    constexpr int kVersionMajor = ICTK_VERSION_MAJOR;
+    constexpr int kVersionMinor = ICTK_VERSION_MINOR;
+    constexpr int kVersionPatch = ICTK_VERSION_PATCH;
+    constexpr char kVersionStr[] = "0.0.2"; 
+    constexpr int kCAbiVersion = ICTK_C_ABI_VER;
+} // namespace ictk
