d937daa60dccc0ea783bda666a97ccb35e9c4153
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-08-15 13:39:25 +0530

    time

---
+++ include/ictk/core/time.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+
+namespace ictk{
+    // // Standardized time unit across whole toolkit in Nanoseconds
+
+    using t_ns = std::int64_t;
+    using dt_ns = std::int64_t;
+
+} // namespace ictk
