209d320c38bba95f627f41ed76ea1a05b7ebdd13
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-08-25 20:31:11 +0530

    allocation free bitmask gate

--- include/ictk/safety/interlocks.hpp
+++ include/ictk/safety/interlocks.hpp
@@ -1,34 +1,42 @@
 #pragma once
 #include <cstdint>
 
+// // allocation free bitmask gate -> actuation is allowed iff all required bits are ON.
 namespace ictk::safety{
     // // bitmask interlocks: all required bits must be set to allow actuation
     class Interlocks{
         public:
+            // // preventing implicit conversion int to InterLocks: Interlocks il(...) ; not Interlocks il = X; X = some numer 1,2,3...
             explicit Interlocks(
-                std::uint64_t required_mask = ~0ull
+                std::uint64_t required_mask = ~0ull  // // all 64 bits set to 1
             ) noexcept : required_(required_mask) {}
 
+            // // set which bits are mandatory
             void set_required(std::uint64_t mask) noexcept{
                 required_ = mask;
             }
 
+            // // | bitwise OR assignment -> turns on the given bit 
             void set(std::uint64_t mask) noexcept{
                 bits_ |= mask;
             }
 
+            // // & bitwise AND assignment | ~mask: bitwise NOT, turns 1->0 and 0->1, so &= ~mask turns off the given bit
             void clear(std::uint64_t mask) noexcept{
                 bits_ &= ~mask;
             }
 
+            // // set or clear by condition
             void write(std::uint64_t mask, bool on) noexcept{
                 on ? set(mask) : clear(mask);
             }
 
+            // // ensures all required bits are present 
             bool ok() const noexcept{
                 return (bits_ & required_) == required_;
             }
 
+            // // helper functions
             std::uint64_t bits() const noexcept{
                 return bits_;
             }
@@ -39,5 +47,9 @@ namespace ictk::safety{
 
         private:
             std::uint64_t bits_{0}, required_{0};
+            /*
+            bits: current state of all interlocks (1 = satisfied)
+            required : which bits must be 1 to allow action
+            */
     };
 } // namespace ictk::safety
