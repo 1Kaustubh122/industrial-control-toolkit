a51e217452d036b0edd5bbfc6990edf6403eb2ff
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-10-05 23:04:38 +0530

    ictk: version fix

--- include/ictk/version.hpp
+++ include/ictk/version.hpp
@@ -1,21 +1,25 @@
 #pragma once
+
 #define ICTK_VERSION_MAJOR 0
 #define ICTK_VERSION_MINOR 0
 #define ICTK_VERSION_PATCH 2
-#define ICTK_C_ABI_VER 1
+#define ICTK_VERSION_STR   "0.0.2"
+#define ICTK_C_ABI_VER     1
 
 
-namespace ictk{
-    constexpr int kVersionMajor = ICTK_VERSION_MAJOR;
-    constexpr int kVersionMinor = ICTK_VERSION_MINOR;
-    constexpr int kVersionPatch = ICTK_VERSION_PATCH;
-    constexpr char kVersionStr[] = "0.0.2"; 
-    constexpr int kCAbiVersion = ICTK_C_ABI_VER;
+namespace ictk {
+    constexpr int  kVersionMajor = ICTK_VERSION_MAJOR;
+    constexpr int  kVersionMinor = ICTK_VERSION_MINOR;
+    constexpr int  kVersionPatch = ICTK_VERSION_PATCH;
+    constexpr char kVersionStr[] = ICTK_VERSION_STR;
+    constexpr int  kCAbiVersion  = ICTK_C_ABI_VER;
 } // namespace ictk
 
-
-extern "C" {
-    // C ABI 
-    const char* ictk_version_string();
-    int         ictk_c_abi_version();
+#if defined(__cplusplus)
+extern "C"{
+#endif
+    const char* ictk_version_string() noexcept;
+    int         ictk_c_abi_version() noexcept;
+#if defined(__cplusplus)
 }
+#endif
--- src/ictk_version.cc
+++ src/ictk_version.cc
@@ -1,9 +1,12 @@
-#include <ictk/version.hpp>
+#include "ictk/version.hpp"
+#include "ictk/visibility.hpp"
 
-extern "C" const char* ictk_version_string(){
-    return ictk::kVersionStr;
-}
+extern "C"{
+    ICTK_API const char* ictk_version_string() noexcept {
+        return ictk::kVersionStr;
+    }
 
-extern "C" int ictk_c_abi_version(){
-    return ictk::kCAbiVersion;
+    ICTK_API int ictk_c_abi_version() noexcept{
+        return ictk::kCAbiVersion;
+    }
 }
